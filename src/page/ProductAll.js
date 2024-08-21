78de5cc4f6f0e4fc2580b4abb8c9b2b072baaa4e
wonho111 <89952468+cwonho11@users.noreply.github.com>
2024-08-21 23:25:17 +0900

    my-json 서버 이용을 위해 링크 수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -4,7 +4,7 @@ import ProductCard from '../component/ProductCard';
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
     const getProducts = async () => {
-        let url = `http://localhost:5000/products`;
+        let url = `https://my-json-server.typicode.com/cwonho11/ho_react_study/products`;
         let response = await fetch(url);
         let data = await response.json();
 
