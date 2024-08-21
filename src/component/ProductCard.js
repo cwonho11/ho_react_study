b52780f0be89312e4c4b4a363af026f5439f734f
wonho111 <wonho111@gmail.com>
2024-08-21 22:56:07 +0900

    5강까지 진행사항 커밋

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://noona-hnm.netlify.app/pattern-jacket.jpeg" />
+        <div>choice</div>
+        <div>코트</div>
+        <div>99000</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard;
